f77c2ba4e8af894658aa380584bf5b43ae12b110
Dhaulan <amitkumar025@gmail.com>
2024-05-06 18:15:13 +0530

    initial commit

---
+++ src/test/java/org/example/LaunchBrowser.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+    @Test
+    public void launchBrowser() {
+        EdgeDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+
+    }
+}
